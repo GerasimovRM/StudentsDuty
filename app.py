dde8e57f94960235a387830f43376a763505309e
Roman M. Gerasimov <romagrizly@gmail.com>
2020-03-19 18:20:16 +0500

    Update app

--- app.py
+++ app.py
@@ -1,18 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
 Bootstrap(app)
-app.config['BOOTSTRAP_SERVE_LOCAL'] = True
 
 
 @app.route('/')
-def hello_world():
-    return render_template("index.html")
+@app.route('/index')
+def index():
+    return render_template("index.html", title='Главная')
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html", title='О нас')
+
+
+@app.route('/right')
+def right():
+    return render_template("sidebar-right.html", title='1')
+
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html", title='Контакты')
+
+
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+    return render_template("signin.html", title='Войти')
+
+
+@app.route('/signup')
+def signup():
+    return render_template("signup.html", title='Регистрация')
 
 
 if __name__ == "__main__":
-    app.run(ssl_context='adhoc')
+    app.run()
 
 
