dbbfa627d056c6c6d98b38423c9538dcf70c2477
Roman M. Gerasimov <romagrizly@gmail.com>
2020-03-20 05:14:19 +0500

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def signup():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 33507))
     app.run(host='0.0.0.0', port=port)
     # app.run(host='127.0.0.1', port=port)
 
